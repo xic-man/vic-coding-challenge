1dc41966e0ad8286709551358f366cb12dc8cde0
xic-man <58388457+xic-man@users.noreply.github.com>
2020-11-29 17:16:56 +1100

    Updated tensorflow info and warning blocker

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 # code from https://github.com/timschott/gpt-2
 import numpy as np
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow.compat.v1 as tf
 tf.get_logger().setLevel('ERROR')
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
